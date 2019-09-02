ed40b9120fcc6b67efbafbc77a4252467051fe73
Bikash Pokhrel <bpokhrel@aut.ac.nz>
2019-09-03 09:42:37 +1200

    Add project files.

---
+++ Class Calc/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Class_Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.cbxOperation = new System.Windows.Forms.ComboBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(445, 216);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(102, 45);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(432, 132);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(552, 132);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.ItemHeight = 16;
+            this.lbxOutput.Location = new System.Drawing.Point(142, 132);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(255, 260);
+            this.lbxOutput.TabIndex = 3;
+            // 
+            // cbxOperation
+            // 
+            this.cbxOperation.FormattingEnabled = true;
+            this.cbxOperation.Location = new System.Drawing.Point(581, 227);
+            this.cbxOperation.Name = "cbxOperation";
+            this.cbxOperation.Size = new System.Drawing.Size(121, 24);
+            this.cbxOperation.TabIndex = 4;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblAnswer.Location = new System.Drawing.Point(149, 77);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(225, 52);
+            this.lblAnswer.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.cbxOperation);
+            this.Controls.Add(this.lbxOutput);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ListBox lbxOutput;
+        private System.Windows.Forms.ComboBox cbxOperation;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ Class Calc/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class_Calc
+{
+    public partial class Form1 : Form
+    {
+        //create a new instance of the Calc class
+        private Calc myCalc = new Calc();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCombobox();
+        }
+        private void LoadCombobox()
+        {
+            //pass the array across to the combobox
+            cbxOperation.Items.AddRange(myCalc.LoadSymbols());
+            //start at the first item
+            cbxOperation.SelectedIndex = 0;
+        }
+
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            double result;
+            //Check for numbers coming in
+            if (double.TryParse(txtNum1.Text, out result))
+            {
+                myCalc.Num1 = result;
+            }
+            if (double.TryParse(txtNum2.Text, out result))
+            {
+                myCalc.Num2 = result;
+            }
+            //check selected operation
+            myCalc.SelectedSymbol = cbxOperation.SelectedItem.ToString();
+            //output answer
+            lblAnswer.Text = Convert.ToString(myCalc.SelectOperation());
+            lbxOutput.Items.Add(myCalc.Num1 + " " + myCalc.SelectedSymbol + " " + myCalc.Num2 + " = " + lblAnswer.Text);
+        }
+    }
+    
+}
